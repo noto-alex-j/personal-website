b470b3429c4090db879e5dbe3ae742cdb63ab2d5
Alexander Noto <noto.alex.j@gmail.com>
2016-06-24 14:41:44 -0500

    Added css sheet

--- navigation/navigation.php
+++ navigation/navigation.php
@@ -11,9 +11,11 @@ sort($articlelist);
 ?>
 
 <div id= "linkcontainer">
+
+<div class= "headercontainer">
 <h3>Links</h3>
 
-<ul>
+<ul class= "listcontainer">
 	
 	<li><a href= "<?php if ($prefix == ""){echo "Home.php";} else {echo "../Home.php";} ?>">Home</a></li>
 	<br>
@@ -22,24 +24,29 @@ sort($articlelist);
 	<br>
 <?php } ?>
 </ul>
+</div>
 
+<div class= "headercontainer">
 <h3>Articles</h3>
 
-<ul>
+<ul class= "listcontainer">
 <?php for ($file = 3; $file < count($articlelist); $file++) { ?>
 		<li><a href= "<?php if ($prefix == ""){echo "articles/";} else {echo "../articles/";}?><?php echo ltrim($articlelist[$file], "//articles/"); ?>"><?php echo rtrim($articlelist[$file], ".php"); ?></a></li>
 		<br>
 <?php } ?>
 </ul>
+</div>
 
-
+<div class= "headercontainer">
 <h3>Social Media</h3>
 
-<ul>
+<ul class= "listcontainer">
 	<li><a href="https://www.facebook.com/alex.noto">Facebook</a></li>
 	<br>
 	<li><a href="https://github.com/noto-alex-j">Github</a></li>
 </ul>
+</div>
+
 </div>
 
 <?php
