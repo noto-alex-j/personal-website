ca719bb9a83574467c2e56ad88234225c215fca0
Alexander Noto <noto.alex.j@gmail.com>
2016-06-25 12:17:55 -0500

    formatted drop down

--- navigation/navigation.php
+++ navigation/navigation.php
@@ -19,14 +19,12 @@ sort($articlelist);
 
 <div class= "dropdown">
 
-	<h3 class= "dropdownbutton">Links</h3>
+	<div class= "dropdownbutton"><h3>Links</h3></div>
 
 	<div class= "dropdowncontent">
 		<a href= "<?php if ($prefix == ""){echo "Home.php";} else {echo "../Home.php";} ?>">Home</a>
-		<br>
 		<?php for ($file = 3; $file < count($linkslist); $file++) { ?>
 		<a href= "<?php if ($prefix == ""){echo "links/";} else {echo "../links/";}?><?php echo ltrim($linkslist[$file], "//links/"); ?>"><?php echo rtrim($linkslist[$file], ".php"); ?></a>
-		<br>
 		<?php } ?>
 	</div>
 
@@ -36,12 +34,11 @@ sort($articlelist);
 
 <div class= "dropdown">
 
-	<h3 class= "dropdownbutton">Articles</h3>
+	<div class= "dropdownbutton"><h3>Articles</h3></div>
 
 	<div class= "dropdowncontent">
 		<?php for ($file = 3; $file < count($articlelist); $file++) { ?>
 		<a href= "<?php if ($prefix == ""){echo "articles/";} else {echo "../articles/";}?><?php echo ltrim($articlelist[$file], "//articles/"); ?>"><?php echo rtrim($articlelist[$file], ".php"); ?></a>
-		<br>
 		<?php } ?>
 	</div>
 
@@ -51,11 +48,10 @@ sort($articlelist);
 
 <div class= "dropdown">
 
-	<h3 class= "dropdownbutton">Social Media</h3>
+	<div class= "dropdownbutton"><h3>Social Media</h3></div>
 
 	<div class= "dropdowncontent">
 		<a href="https://www.facebook.com/alex.noto">Facebook</a>
-		<br>
 		<a href="https://github.com/noto-alex-j">Github</a>
 	</div>
 
