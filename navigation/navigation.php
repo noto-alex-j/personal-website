a8739f836ef3c1a228ba47984f517904c2ccca52
Alexander Noto <noto.alex.j@gmail.com>
2016-06-25 11:03:47 -0500

    indentation

--- navigation/navigation.php
+++ navigation/navigation.php
@@ -14,44 +14,55 @@ sort($articlelist);
 <div id= "overalllinkcontainer">
 
 <!-- ----------------------------------------------------------------------------------------------- -->
-<!-- -----Links List Subcontainer----- -->
-<div class= "headerandlistcontainer">
-<h3 class= "listcontainerbutton">Links</h3>
 
-<div class= "listcontainer">
-	
-	<a href= "<?php if ($prefix == ""){echo "Home.php";} else {echo "../Home.php";} ?>">Home</a>
-	<br>
-<?php for ($file = 3; $file < count($linkslist); $file++) { ?>
-	<a href= "<?php if ($prefix == ""){echo "links/";} else {echo "../links/";}?><?php echo ltrim($linkslist[$file], "//links/"); ?>"><?php echo rtrim($linkslist[$file], ".php"); ?></a>
-	<br>
-<?php } ?>
-</div>
+<!-- -----Links List Subcontainer----- -->
+
+<div class= "dropdown">
+
+	<h3 class= "dropdownbutton">Links</h3>
+
+	<div class= "dropdowncontent">
+		<a href= "<?php if ($prefix == ""){echo "Home.php";} else {echo "../Home.php";} ?>">Home</a>
+		<br>
+		<?php for ($file = 3; $file < count($linkslist); $file++) { ?>
+		<a href= "<?php if ($prefix == ""){echo "links/";} else {echo "../links/";}?><?php echo ltrim($linkslist[$file], "//links/"); ?>"><?php echo rtrim($linkslist[$file], ".php"); ?></a>
+		<br>
+		<?php } ?>
+	</div>
+
 </div>
 
 <!-- -----Articles List Subcontainer----- -->
-<div class= "headerandlistcontainer">
-<h3 class= "listcontainerbutton">Articles</h3>
 
-<div class= "listcontainer">
-<?php for ($file = 3; $file < count($articlelist); $file++) { ?>
+<div class= "dropdown">
+
+	<h3 class= "dropdownbutton">Articles</h3>
+
+	<div class= "dropdowncontent">
+		<?php for ($file = 3; $file < count($articlelist); $file++) { ?>
 		<a href= "<?php if ($prefix == ""){echo "articles/";} else {echo "../articles/";}?><?php echo ltrim($articlelist[$file], "//articles/"); ?>"><?php echo rtrim($articlelist[$file], ".php"); ?></a>
 		<br>
-<?php } ?>
-</div>
+		<?php } ?>
+	</div>
+
 </div>
 
 <!-- -----Social Media List Subcontainer----- -->
-<div class= "headerandlistcontainer">
-<h3 class= "listcontainerbutton">Social Media</h3>
 
-<div class= "listcontainer">
-	<a href="https://www.facebook.com/alex.noto">Facebook</a>
-	<br>
-	<a href="https://github.com/noto-alex-j">Github</a>
+<div class= "dropdown">
+
+	<h3 class= "dropdownbutton">Social Media</h3>
+
+	<div class= "dropdowncontent">
+		<a href="https://www.facebook.com/alex.noto">Facebook</a>
+		<br>
+		<a href="https://github.com/noto-alex-j">Github</a>
+	</div>
+
 </div>
-</div>
+
 <!-- ------------------------------------------------------------------------------------------------ -->
+
 </div>
 
 <?php
