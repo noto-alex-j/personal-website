7863965831ac984ecc86e6e0a5a106702528ec1d
Alexander Noto <noto.alex.j@gmail.com>
2016-06-25 19:52:27 -0500

    attempting link background

--- links/Goals.php
+++ links/Goals.php
@@ -12,33 +12,41 @@
 
 <body>
 
-<a href= "/Home.php"><h1>Alex Noto</h1></a>
-<hr>
+<div id= "pagelayoutcontainer">
 
-<h3>Personal Goals</h3>
-<ul>
-	<li>Improve my time management skills</li>
-	<br>
-	<li>Get back into reading</li>
-	<br>
-	<li>Do more sketching and painting</li>
-	<br>
-	<li>Travel more!</li>
-</ul>
+	<div id= "page">
 
-<h3>Professional Goals</h3>
-<ul>
-	<li>Develop my programming skills</li>
-	<br>
-	<li>Learn to build well-crafted products</li>
-	<br>
-	<li>Network with others in my field</li>
-	<br>
-	<li>Enjoy my job!</li>
-</ul>
+		<div id= "pagecontents">
 
+			<h3>Personal Goals</h3>
+			<ul>
+				<li>Improve my time management skills</li>
+				<br>
+				<li>Get back into reading</li>
+				<br>
+				<li>Do more sketching and painting</li>
+				<br>
+				<li>Travel more!</li>
+			</ul>
 
-<?php include("../navigation/navigation.php"); ?>
+			<h3>Professional Goals</h3>
+			<ul>
+				<li>Develop my programming skills</li>
+				<br>
+				<li>Learn to build well-crafted products</li>
+				<br>
+				<li>Network with others in my field</li>
+				<br>
+				<li>Enjoy my job!</li>
+			</ul>
+
+		</div>
+
+	</div>
+
+	<?php include("../navigation/navigation.php"); ?>
+
+</div>
 
 
 </body>
--- links/Mechanical Mind.php
+++ links/Mechanical Mind.php
@@ -12,27 +12,36 @@
 
 <body>
 
-<a href= "/Home.php"><h1>Alex Noto</h1></a>
-<hr>
+<div id= "pagelayoutcontainer">
 
-<h3>Mechanical Mind</h3>
-<p>
-I'm a car guy. Always have been. It's an industry in which I'd like to work. But until then, here are some links to some of the leading companies I admire:
-</p>
+	<div id= "page">
 
-<a href= "http://www.onassisporsches.com"><img src= "http://www.onassisporsches.com/.cm4all/iproc.php/distressed%20Onassis.png/resize_152_80/distressed%20Onassis.png" /></a>
-<br>
-<br>
-<a href= "http://www.fluidmotorunion.com"><img src= "http://www.fluidmotorunion.com/wp-content/uploads/2015/07/logo.jpg" /></a>
-<br>
-<br>
-<a href="http://www.stanceworks.com"><img src= "http://stancewords.stanceworks.netdna-cdn.com/wp-content/uploads/2012/03/Resizebanner1.png" /></a>
-<br>
-<br>
-<a href= "http://www.elephantracing.com/index.htm"><img src= "http://www.elephantracing.com/images/logo_061813.png" /></a>
+		<div id= "pagecontents">
 
-<?php include("../navigation/navigation.php"); ?>
+			<h3>Mechanical Mind</h3>
 
+			<p>
+				I'm a car guy. Always have been. It's an industry in which I'd like to work. But until then, here are some links to some of the leading companies I admire:
+			</p>
+
+			<a href= "http://www.onassisporsches.com"><img src= "http://www.onassisporsches.com/.cm4all/iproc.php/distressed%20Onassis.png/resize_152_80/distressed%20Onassis.png" /></a>
+			<br>
+			<br>
+			<a href= "http://www.fluidmotorunion.com"><img src= "http://www.fluidmotorunion.com/wp-content/uploads/2015/07/logo.jpg" /></a>
+			<br>
+			<br>
+			<a href="http://www.stanceworks.com"><img src= "http://stancewords.stanceworks.netdna-cdn.com/wp-content/uploads/2012/03/Resizebanner1.png" /></a>
+			<br>
+			<br>
+			<a href= "http://www.elephantracing.com/index.htm"><img src= "http://www.elephantracing.com/images/logo_061813.png" /></a>
+
+		</div>
+
+	</div>
+
+	<?php include("../navigation/navigation.php"); ?>
+
+</div>
 
 </body>
 
