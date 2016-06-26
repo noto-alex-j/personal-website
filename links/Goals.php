0c7455ed3f11d98eab336ddcd2546ef5757752e8
Alexander Noto <noto.alex.j@gmail.com>
2016-06-26 18:51:21 -0500

    finally fixed theme

--- links/Goals.php
+++ links/Goals.php
@@ -6,7 +6,7 @@
 
 <html>
 
-<?php if ($_GET["theme"] == "dark"){echo '<link type= "text/css"; rel= "stylesheet"; href= "stylesheet alt.css">';} elseif ($_GET["theme"] == "light"){echo '<link type= "text/css"; rel= "stylesheet"; href= "stylesheet.css">';} else{echo '<link type= "text/css"; rel= "stylesheet"; href= "/personal-website/stylesheet.css">';} ?>
+<?php if ($_GET["theme"] == "dark"){echo '<link type= "text/css"; rel= "stylesheet"; href= "/stylesheet alt.css">';} elseif ($_GET["theme"] == "light"){echo '<link type= "text/css"; rel= "stylesheet"; href= "/stylesheet.css">';} else{echo '<link type= "text/css"; rel= "stylesheet"; href= "/stylesheet.css">';} ?>
 
 <head>
 <title>Goals</title>
