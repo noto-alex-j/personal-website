9c0e238c4cb34fdb9dccf94e75f3e25e283e69bc
Alexander Noto <noto.alex.j@gmail.com>
2016-06-30 19:21:28 -0500

    Almost figured TTT code out

--- tttfiles/ttt-board.php
+++ tttfiles/ttt-board.php
@@ -3,27 +3,31 @@
 <div class= "tttbox">
 
 	<div class= "row">
-		<a href= "?board=<?php echo $xsquare1;?>"><div class= "square"><?php echo $board[0]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare2;?>"><div class= "square"><?php echo $board[1]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare3;?>"><div class= "square"><?php echo $board[2]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare1;?>"><div class= "square"><?php echo $boardarrangementnew[0]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare2;?>"><div class= "square"><?php echo $boardarrangementnew[1]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare3;?>"><div class= "square"><?php echo $boardarrangementnew[2]; ?></div></a>
 	</div>
 
 	<div class= "row">
-		<a href= "?board=<?php echo $xsquare4;?>"><div class= "square"><?php echo $board[3]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare5;?>"><div class= "square"><?php echo $board[4]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare6;?>"><div class= "square"><?php echo $board[5]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare4;?>"><div class= "square"><?php echo $boardarrangementnew[3]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare5;?>"><div class= "square"><?php echo $boardarrangementnew[4]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare6;?>"><div class= "square"><?php echo $boardarrangementnew[5]; ?></div></a>
 	</div>
 
 	<div class= "row">
-		<a href= "?board=<?php echo $xsquare7;?>"><div class= "square"><?php echo $board[6]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare8;?>"><div class= "square"><?php echo $board[7]; ?></div></a>
-		<a href= "?board=<?php echo $xsquare9;?>"><div class= "square"><?php echo $board[8]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare7;?>"><div class= "square"><?php echo $boardarrangementnew[6]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare8;?>"><div class= "square"><?php echo $boardarrangementnew[7]; ?></div></a>
+		<a href= "?board=<?php echo $xsquare9;?>"><div class= "square"><?php echo $boardarrangementnew[8]; ?></div></a>
 	</div>
 
 
 </div>
 
-<?php echo queryreplace($firstmove,$secondmove); ?>
 
-
-
+<a href="Tic%20Tac%20Toe.php">Reset</a>
+<br>
+<?php print_r($boardarrangement); ?>
+<br>
+<?php echo $_GET["board"]; ?>
+<br>
+<?php print_r($boardarrangementnew); ?>
--- tttfiles/ttt-code.php
+++ tttfiles/ttt-code.php
@@ -1,38 +1,30 @@
 <?php
 
-/* Sets the board equal to 9 digit parameter */
-$board = $_GET["board"];
-$board = str_split($board);
-
-
-/*----------------------------------------*/
-
 $player1 = "X";
 $player2 = "O";
 
+/* ------ List of box links for Player 1 and Player 2 ------ */
 
-$xsquare1 = "$player1        ";
-$xsquare2 = " $player1       ";
-$xsquare3 = "  $player1      ";
-$xsquare4 = "   $player1     ";
-$xsquare5 = "    $player1    ";
-$xsquare6 = "     $player1   ";
-$xsquare7 = "      $player1  ";
-$xsquare8 = "       $player1 ";
-$xsquare9 = "        $player1";
+$xsquare1 = $player1 . "--------";
+$xsquare2 = "-" . $player1 . "-------";
+$xsquare3 = "--" . $player1 . "------";
+$xsquare4 = "---" . $player1 . "-----";
+$xsquare5 = "----" . $player1 . "----";
+$xsquare6 = "-----" . $player1 . "---";
+$xsquare7 = "------" . $player1 . "--";
+$xsquare8 = "-------" . $player1 . "-";
+$xsquare9 = "--------" . $player1;
 
-$osquare1 = "$player2        ";
-$osquare2 = " $player2       ";
-$osquare3 = "  $player2      ";
-$osquare4 = "   $player2     ";
-$osquare5 = "    $player2    ";
-$osquare6 = "     $player2   ";
-$osquare7 = "      $player2  ";
-$osquare8 = "       $player2 ";
-$osquare9 = "        $player2";
+$osquare1 = $player2 . "--------";
+$osquare2 = "-" . $player2 . "-------";
+$osquare3 = "--" . $player2 . "------";
+$osquare4 = "---" . $player2 . "-----";
+$osquare5 = "----" . $player2 . "----";
+$osquare6 = "-----" . $player2 . "---";
+$osquare7 = "------" . $player2 . "--";
+$osquare8 = "-------" . $player2 . "-";
+$osquare9 = "--------" . $player2;
 
-
-$firstmove = "         ";
 
 /* ---- Takes the current board layout query and the new board layout query and merges them ---- */
 
@@ -40,7 +32,7 @@ function queryreplace($first,$second) {
 	$first = str_split($first);
 	$second = str_split($second);
 	foreach ($first as $firstkey => $boardarrange) {
-		if ($first[$firstkey] == " " && $second[$firstkey] == "X" || "O") {
+		if ($first[$firstkey] == "-" && $second[$firstkey] == ("X" || "O")) {
 			if ($first[$firstkey] == "X" && $second[$firstkey] == "O" || $first[$firstkey] == "O" && $second[$firstkey] == "X") {
 			}
 			else {
@@ -51,15 +43,9 @@ function queryreplace($first,$second) {
 	return $first;
 }
 
-/* $secondmove[] = array_diff($firstmove, $secondmove);
-
-foreach ($secondmove as $square => $boardarrangement) {
-	
-} */
-
-
-
-
+$boardarrangement = "---------";
+$boardarrangementnew = queryreplace($boardarrangement,$_GET["board"]);
+$playermove = $_GET["board"];
 
 
 ?>
