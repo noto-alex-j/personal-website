986249fae9121b4903c54afc2b976fb5ff83991d
Alexander Noto <noto.alex.j@gmail.com>
2016-07-02 13:05:05 -0500

    Refactoring

--- tttfiles/ttt-code.php
+++ tttfiles/ttt-code.php
@@ -6,42 +6,14 @@ $player2 = "O";
 
 $currentboard = implode($_GET);
 
-/* ----Takes $_GET and adds appropriate query ending, to be added to end of string to make link---- */
+/* Decided that it's easier to just include the & in the first query */
 
-function linkappender1 ($player1) {
-	$currentboard = implode($_GET);
-	foreach ($player1 as $boxnumber => $entry) {
-		if ($currentboard == "") {
-			$player1[$boxnumber] = $entry . "=X";
-		}
-		else {
-			$player1[$boxnumber] = "&" . $entry . "=X";
-		}
-	}
-	return $player1;
-}
+$links1 = array (
+	0=>"&0=X",1=>"&1=X",2=>"&2=X",3=>"&3=X",4=>"&4=X",5=>"&5=X",6=>"&6=X",7=>"&7=X",8=>"&8=X");
 
-function linkappender2 ($player2) {
-	$currentboard = implode($_GET);
-	foreach ($player2 as $boxnumber => $entry) {
-		if ($currentboard == "") {
-			$player2[$boxnumber] = $entry . "=O";
-		}
-		else {
-			$player2[$boxnumber] = "&" . $entry . "=O";
-		}
-	}
-	return $player2;
-}
+$links2 = array (
+	0=>"&0=O",1=>"&1=O",2=>"&2=O",3=>"&3=O",4=>"&4=O",5=>"&5=O",6=>"&6=O",7=>"&7=O",8=>"&8=O");
 
-/* Player 1 and 2 arrays fed into link appender to generate appropriate link endings */
-/* The generated link endings are then affixed to the existing $_GET string and become new hrefs */
-
-$player1 = array(0,1,2,3,4,5,6,7,8);
-$player2 = array(0,1,2,3,4,5,6,7,8);
-
-$links1 = linkappender1($player1);
-$links2 = linkappender2($player2);
 
 /* ------ Switches player turn based on last element in query param ------ */
 
@@ -81,40 +53,21 @@ $owin = array(
 );
 
 /* Checks for winning X arrangement and if present returns Player 1 Wins */
-/* Prevents double reporting */
-
-/* function xwinnercheck($GET, $win) {
-	$GET = $_GET;
-	foreach ($win as $winningarray) {
-		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
-			echo "Player 1 Wins";
-		}
-	}
-}
-
-function owinnercheck($GET, $win) {
-	$GET = $_GET;
-	foreach ($win as $winningarray) {
-		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
-			echo "Player 2 Wins";
-		}
-	}
-} */
-
+/* Still double reporting */
 
 function winnercheck($GET, $xwin, $owin) {
-	$winner = "";
 	$GET = $_GET;
+	$winner = "";
 	foreach ($xwin as $winningarray) {
 		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
-			if ($winner == "") {
+			if ($winner === "") {
 				$winner = "Player 1 Wins";
 			}
 		}
 	}
 	foreach ($owin as $winningarray) {
 		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
-			if ($winner == "") {
+			if ($winner === "") {
 				$winner = "Player 2 Wins";
 			}
 		}
@@ -122,7 +75,7 @@ function winnercheck($GET, $xwin, $owin) {
 	return $winner;
 }
 
-
+/* -------------------------------- Unused Code --------------------------------------- 
 
 /* ------ List of box links for Player 1 and Player 2 ------ 
 
@@ -148,7 +101,7 @@ $osquare9 = "--------O";
 
 
 
- ---- Takes the current board layout query and the new board layout query and merges them ---- 
+ /* ---- Takes the current board layout query and the new board layout query and merges them ---- 
 
 function boardpositionchanger($first,$second) {
 	$first = str_split($first);
@@ -165,9 +118,64 @@ function boardpositionchanger($first,$second) {
 	return $first;
 }
 
+
+
+ ----Takes $_GET and adds appropriate query ending, to be added to end of string to make link----
+
+function linkappender1 ($player1) {
+	$currentboard = implode($_GET);
+	foreach ($player1 as $boxnumber => $entry) {
+		if ($currentboard == "") {
+			$player1[$boxnumber] = $entry . "=X";
+		}
+		else {
+			$player1[$boxnumber] = "&" . $entry . "=X";
+		}
+	}
+	return $player1;
+}
+
+function linkappender2 ($player2) {
+	$currentboard = implode($_GET);
+	foreach ($player2 as $boxnumber => $entry) {
+		if ($currentboard == "") {
+			$player2[$boxnumber] = $entry . "=O";
+		}
+		else {
+			$player2[$boxnumber] = "&" . $entry . "=O";
+		}
+	}
+	return $player2;
+}
+
+/* Player 1 and 2 arrays fed into link appender to generate appropriate link endings 
+/* The generated link endings are then affixed to the existing $_GET string and become new hrefs
+
+$player1 = array(0,1,2,3,4,5,6,7,8);
+$player2 = array(0,1,2,3,4,5,6,7,8);
+
+$links1 = linkappender1($player1);
+$links2 = linkappender2($player2); 
+
+
+ /* function xwinnercheck($GET, $xwin) {
+	$GET = $_GET;
+	foreach ($xwin as $winningarray) {
+		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
+			echo "Player 1 Wins";
+		}
+	}
+}
+
+function owinnercheck($GET, $owin) {
+	$GET = $_GET;
+	foreach ($owin as $winningarray) {
+		if (array_intersect_assoc($GET, $winningarray) == $winningarray) {
+			echo "Player 2 Wins";
+		}
+	}
+}
+
+
 */
-
-
-
-
 ?>
