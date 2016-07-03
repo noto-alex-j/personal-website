c0e068b39b80d322ecabdbc7c64b73ebcd1df34c
Alexander Noto <noto.alex.j@gmail.com>
2016-07-03 18:17:03 -0500

    test edit

--- tttfiles/ttt-test.php
+++ tttfiles/ttt-test.php
@@ -1,10 +1,10 @@
-<?php include ("ttt-code.php"); ?>
+<?php include ("ttt-code-dual-player.php"); ?>
 
 <?php
-echo "Testing result of board configuration $GET = (0=>"X",1=>"O",3=>"X",4=>"O",6=>"X"):"
-$GET = array(0=>"X",1=>"O",3=>"X",4=>"O",6=>"X");
-if winnercheck($GET, $xwin, $owin) == "Player 1 Wins") {
-	echo "Success!";
+echo nl2br ('Testing result of board configuration $GET = (0=>"X",1=>"O",3=>"X",4=>"O",6=>"X"):');
+$_GET = array(0=>"X",1=>"O",3=>"X",4=>"O",6=>"X");
+if (winnercheck($_GET, $xwin, $owin) == "Player 1 Wins") {
+	echo nl2br ("\n Success! \n");
 }
 else {
 	echo "Failed.";
@@ -12,10 +12,10 @@ else {
 ?>
 
 <?php
-echo "Testing result of board configuration $GET = (0=>"X",2=>"O",3=>"X",4=>"O",6=>"O"):"
-$GET = array(0=>"X",2=>"O",3=>"X",4=>"O",6=>"O");
-if winnercheck($GET, $xwin, $owin) == "Player 2 Wins") {
-	echo "Success!";
+echo 'Testing result of board configuration $GET = (0=>"X",2=>"O",3=>"X",4=>"O",6=>"O"):';
+$_GET = array(0=>"X",2=>"O",3=>"X",4=>"O",6=>"O");
+if (winnercheck($_GET, $xwin, $owin) == "Player 2 Wins") {
+	echo nl2br ("\n Success! \n");
 }
 else {
 	echo "Failed.";
@@ -23,10 +23,10 @@ else {
 ?>
 
 <?php
-echo "Testing result of board configuration $GET = (1=>"O",4=>"X",7=>"X"):"
-$GET = array(1=>"O",4=>"X",7=>"X");
-if winnercheck($GET, $xwin, $owin) == "") {
-	echo "Success!";
+echo 'Testing result of board configuration $GET = (1=>"O",4=>"X",7=>"X"):';
+$_GET = array(1=>"O",4=>"X",7=>"X");
+if (winnercheck($_GET, $xwin, $owin) == "") {
+	echo nl2br ("\n Success!");
 }
 else {
 	echo "Failed.";
